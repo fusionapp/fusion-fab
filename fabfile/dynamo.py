a7a1cf1dc3942d567e4870b79a5428266ae18a39
Darren Dormer <me@darren.io>
2015-10-07 17:29:00 +0200

    Add deploy task. Actually build the dynamo docker

--- fabfile/dynamo.py
+++ fabfile/dynamo.py
@@ -8,7 +8,23 @@ def build_clj_dynamo():
         if run('test -d /srv/build/clj-dynamo').failed:
             run('mkdir -p /srv/build/clj-dynamo')
             run('git clone --quiet https://github.com/ddormer/clj-dynamo.git /srv/build/clj-dynamo/src')
-    with cd('/srv/build/fusion'):
-        run('hg pull -u')
+    with cd('/srv/build/clj-dynamo/src'):
         run('docker pull idnar/lein')
         run('docker run --rm --tty --interactive --volume=/srv/build/clj-dynamo/.m2:/root/.m2 --volume=/srv/build/clj-dynamo/.lein:/root/.lein --volume=/srv/build/clj-dynamo/src:/lein idnar/lein uberjar')
+        run('docker build --tag=fusionapp/dynamo --file=Dockerfile .')
+
+
+@task
+@hosts('root@scarlet.fusionapp.com')
+def deploy():
+    with settings(warn_only=True):
+        run('docker stop --time=30 dynamo')
+        run('docker rm --volumes --force dynamo')
+    run('docker run --detach --restart=always --name=dynamo --publish=8080:8080 --volume="/srv/db/dynamo:/db" --workdir=/db fusionapp/dynamo --config /db/config.json')
+
+
+@task(default=True)
+@hosts('root@scarlet.fusionapp.com')
+def build_and_deploy():
+    build_clj_dynamo()
+    deploy()
