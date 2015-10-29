1cd0f69e5a32f36f61cfdb676befcf12328666cf
Tristan Seligmann <mithrandi@mithrandi.net>
2015-10-29 06:33:29 +0200

    Blah blah.

--- fabfile/fusion/uat.py
+++ fabfile/fusion/uat.py
@@ -5,20 +5,24 @@ from fabric.api import cd, hosts, run, settings, task
 @hosts('root@scarlet.fusionapp.com')
 def build():
     with settings(warn_only=True):
-        if run('test -d /srv/build/fusion').failed:
-            run('hg clone --quiet ssh://hg@bitbucket.org/fusionapp/fusion /srv/build/fusion')
-    with cd('/srv/build/fusion'):
+        if run('test -d /srv/build/fusion/uat').failed:
+            run('mkdir -p /srv/build/fusion')
+            run('hg clone --quiet ssh://hg@bitbucket.org/fusionapp/fusion /srv/build/fusion/uat')
+    with cd('/srv/build/fusion/uat'):
         run('hg pull')
         run('hg update uat')
-        run('docker build --tag=fusionapp/fusion-base --file=docker/base.docker .')
-        run('docker build --tag=fusionapp/fusion-build --file=docker/build.docker .')
-        run('cp -u /srv/build/documint/wheelhouse/* /srv/build/fusion/wheelhouse')
-        run('cp -u /srv/build/diamond/wheelhouse/* /srv/build/fusion/wheelhouse')
+        run('docker build --tag=fusionapp/fusion-base:uat --file=docker/base.docker .')
+        run('docker build --tag=fusionapp/fusion-build:uat --file=docker/build.docker .')
+        run('mkdir -p /srv/build/fusion/uat/wheelhouse')
+        run('cp -u /srv/build/documint/wheelhouse/* /srv/build/fusion/uat/wheelhouse')
+        run('cp -u /srv/build/diamond/wheelhouse/* /srv/build/fusion/uat/wheelhouse')
         # XXX: This is really dumb, but it should be resolved by merging our
         # fork of Nevow upstream.
-        run('rm /srv/build/fusion/wheelhouse/Nevow*')
-        run('docker run --rm --tty --interactive --volume=/srv/build/fusion:/application --volume=/srv/build/fusion/wheelhouse:/wheelhouse fusionapp/fusion-build')
-        run('docker build --tag=fusionapp/fusion --file=docker/run.docker .')
+        run('rm /srv/build/fusion/uat/wheelhouse/Nevow*')
+        run('docker run --rm --tty --interactive --volume=/srv/build/fusion/uat:/application --volume=/srv/build/fusion/uat/wheelhouse:/wheelhouse fusionapp/fusion-build:uat')
+        run('docker build --tag=fusionapp/fusion:uat --file=docker/run.docker .')
+        run('docker tag -f fusionapp/fusion:uat scarlet.fusionapp.com:5000/fusionapp/fusion:uat')
+        run('docker push scarlet.fusionapp.com:5000/fusionapp/fusion:uat', shell=False, pty=False)
 
 
 @task
@@ -27,9 +31,8 @@ def deploy():
     with settings(warn_only=True):
         run('docker stop --time=30 fusion')
         run('docker rm --volumes --force fusion')
-    run('docker run --rm --tty --interactive --volume=/srv/db/fusion:/db --volume=/srv/certs:/srv/certs fusionapp/fusion upgrade')
-    run('docker run --detach --restart=always --name=fusion --volume=/srv/db/fusion:/db --volume=/srv/certs:/srv/certs --publish=80:80 --publish=2233:23 --workdir=/db fusionapp/fusion')
-
+    run('docker run --rm --tty --interactive --volume=/srv/db/fusion:/db --volume=/srv/certs:/srv/certs fusionapp/fusion:uat upgrade')
+    run('docker run --detach --restart=always --name=fusion --volume=/srv/db/fusion:/db --volume=/srv/certs:/srv/certs --publish=80:80 --publish=2233:23 --workdir=/db fusionapp/fusion:uat')
 
 
 @task(default=True)
