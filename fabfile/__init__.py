683b0cb3a5e05e719b543fc8357b957ee6a3cd9d
Jonathan Jacobs <jonathan@jsphere.com>
2015-11-02 22:15:33 +0200

    Fabric task for the Slack-Bitbucket integration.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,5 +1,6 @@
-from . import fusion, documint, diamond, dynamo, tools, fusion_index, registry
+from . import (diamond, documint, dynamo, fusion, fusion_index, registry,
+               slack_better_bitbucket, tools)
 
 __all__ = [
     'fusion', 'documint', 'diamond', 'dynamo', 'tools', 'fusion_index',
-    'registry']
+    'registry', 'slack_better_bitbucket']
