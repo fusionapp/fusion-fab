7741d9af936d371ba9f6c54ead3299135ac97c0e
Jonathan Jacobs <jonathan@jsphere.com>
2015-11-02 23:08:34 +0200

    isort configuration and sort imports.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,5 +1,8 @@
-from . import (diamond, documint, dynamo, fusion, fusion_index, registry,
-               slack_better_bitbucket, tools)
+from . import (
+    diamond, documint, dynamo, fusion, fusion_index, registry,
+    slack_better_bitbucket, tools)
+
+
 
 __all__ = [
     'fusion', 'documint', 'diamond', 'dynamo', 'tools', 'fusion_index',
