290fc7a868fc0a338de0db6ccec0112bc21e2e91
Tristan Seligmann <mithrandi@mithrandi.net>
2015-12-11 13:55:28 +0200

    Tasks for entropy.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,9 +1,9 @@
 from . import (
-    diamond, documint, dynamo, fusion, fusion_index, registry,
+    diamond, documint, dynamo, entropy, fusion, fusion_index, registry,
     slack_better_bitbucket, tools)
 
 
 
 __all__ = [
     'fusion', 'documint', 'diamond', 'dynamo', 'tools', 'fusion_index',
-    'registry', 'slack_better_bitbucket']
+    'registry', 'slack_better_bitbucket', 'entropy']
