c836eb0b8b78b7363eb92e0f17e45174a69c3a04
Tristan Seligmann <mithrandi@mithrandi.net>
2016-02-06 20:43:58 +0200

    Move fusion-index to onyx.

--- fabfile/fusion_index.py
+++ fabfile/fusion_index.py
@@ -13,21 +13,18 @@ def build():
             run('git reset --hard origin/master')
             run('docker run --rm --volume=/srv/build/fusion-index:/application --volume=/srv/build/fusion-index/wheelhouse:/wheelhouse fusionapp/base')
             run('docker build --tag=fusionapp/fusion-index --file=docker/fusion-index.docker .')
+            run('docker tag --force fusionapp/fusion-index scarlet.fusionapp.com:5000/fusionapp/fusion-index')
+            run('docker push scarlet.fusionapp.com:5000/fusionapp/fusion-index')
 
 
 @task
-@hosts('root@scarlet.fusionapp.com')
+@hosts('root@onyx.fusionapp.com')
 def deploy():
     with settings(use_shell=False, always_use_pty=False):
+        run('docker pull scarlet.fusionapp.com:5000/fusionapp/fusion-index')
+        run('docker tag --force scarlet.fusionapp.com:5000/fusionapp/fusion-index fusionapp/fusion-index')
         with settings(warn_only=True):
             run('docker stop --time=30 fusion-index')
             run('docker rm --volumes --force fusion-index')
-        run('docker run --rm --interactive --volume=/srv/db/fusion-index:/db --volume=/srv/certs:/srv/certs fusionapp/fusion-index fusion-index --create --ca=/srv/certs/public/fusion-ca.crt.pem --cert=/srv/certs/private/scarlet.fusionapp.com.pem')
+        run('docker run --rm --volume=/srv/db/fusion-index:/db --volume=/srv/certs:/srv/certs fusionapp/fusion-index fusion-index --create --ca=/srv/certs/public/fusion-ca.crt.pem --cert=/srv/certs/private/onyx.fusionapp.com.pem')
         run('docker run --detach --restart=always --name=fusion-index --volume=/srv/db/fusion-index:/db --volume=/srv/certs:/srv/certs --publish=8443:8443 --log-driver=none --workdir=/db fusionapp/fusion-index start --pidfile "" --nodaemon')
-
-
-@task(default=True)
-@hosts('root@scarlet.fusionapp.com')
-def build_and_deploy():
-    build()
-    deploy()
