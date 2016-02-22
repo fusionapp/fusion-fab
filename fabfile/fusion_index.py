32606bc3f70199bcd87f5fdb4eed815bc342d5a2
Tristan Seligmann <mithrandi@mithrandi.net>
2016-02-22 10:00:57 +0200

    Fix indentation.

--- fabfile/fusion_index.py
+++ fabfile/fusion_index.py
@@ -9,5 +9,5 @@ def deploy():
         with settings(warn_only=True):
             run('docker stop --time=30 fusion-index')
             run('docker rm --volumes --force fusion-index')
-            run('docker run --rm --volume=/srv/db/fusion-index:/db --volume=/srv/certs:/srv/certs:ro scarlet.fusionapp.com:5000/fusionapp/fusion-index:master fusion-index --create --ca=/srv/certs/public/fusion-ca.crt.pem --cert=/srv/certs/private/onyx.fusionapp.com.pem')
-            run('docker run --detach --restart=always --name=fusion-index --volume=/srv/db/fusion-index:/db --volume=/srv/certs:/srv/certs:ro --publish=8443:8443 --log-driver=none scarlet.fusionapp.com:5000/fusionapp/fusion-index:master start --pidfile "" --nodaemon')
+        run('docker run --rm --volume=/srv/db/fusion-index:/db --volume=/srv/certs:/srv/certs:ro scarlet.fusionapp.com:5000/fusionapp/fusion-index:master fusion-index --create --ca=/srv/certs/public/fusion-ca.crt.pem --cert=/srv/certs/private/onyx.fusionapp.com.pem')
+        run('docker run --detach --restart=always --name=fusion-index --volume=/srv/db/fusion-index:/db --volume=/srv/certs:/srv/certs:ro --publish=8443:8443 --log-driver=none scarlet.fusionapp.com:5000/fusionapp/fusion-index:master start --pidfile "" --nodaemon')
