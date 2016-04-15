0c297e2d078ea7d4afbf99ccd56f3c70f0743201
Jonathan Jacobs <jonathan@jsphere.com>
2016-04-15 10:35:40 +0200

    Remove the need to use `docker tag`.

--- fabfile/clj_documint.py
+++ fabfile/clj_documint.py
@@ -6,8 +6,7 @@ from fabric.api import hosts, run, settings, task
 def deploy():
     with settings(use_shell=False, always_use_pty=False):
         run('docker pull scarlet.fusionapp.com:5000/fusionapp/clj-documint')
-        run('docker tag -f scarlet.fusionapp.com:5000/fusionapp/clj-documint fusionapp/clj-documint')
         with settings(warn_only=True):
             run('docker stop --time=30 clj-documint')
             run('docker rm --volumes --force clj-documint')
-        run('docker run --detach --restart=always --name=clj-documint --publish=3000:3000 --volume=/srv/db/clj-documint:/db --workdir=/db fusionapp/clj-documint')
+        run('docker run --detach --restart=always --name=clj-documint --publish=3000:3000 --volume=/srv/db/clj-documint:/db --workdir=/db scarlet.fusionapp.com:5000/fusionapp/clj-documint')
